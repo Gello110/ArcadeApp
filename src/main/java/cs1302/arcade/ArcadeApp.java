34b2f0fee951c808c705aa5cf9010614af5abaa7
ZACHARY MCGEE <mcgee@nike.cs.uga.edu>
2017-11-15 12:47:48 -0500

    Added skeleton code for game, game choice, and start scene.

--- src/main/java/cs1302/arcade/ArcadeApp.java
+++ src/main/java/cs1302/arcade/ArcadeApp.java
@@ -18,43 +18,6 @@ public class ArcadeApp extends Application {
     @Override
     public void start(Stage stage) {
 
-	/* You are allowed to rewrite this start method, add other methods, 
-	 * files, classes, etc., as needed. This currently contains some 
-	 * simple sample code for mouse and keyboard interactions with a node
-	 * (rectangle) in a group. 
-	 */
-	
-	Group group = new Group();           // main container
-	Rectangle r = new Rectangle(20, 20); // some rectangle
-	r.setX(50);                          // 50px in the x direction (right)
-	r.setY(50);                          // 50ps in the y direction (down)
-	group.getChildren().add(r);          // add to main container
-	
-	// when the user clicks on the rectangle, send to random position
-	r.setOnMouseClicked(event -> {
-		System.out.println(event);
-		r.setX(rng.nextDouble() * (640 - r.getWidth()));
-		r.setY(rng.nextDouble() * (480 - r.getHeight()));
-	    });
-
-	// when the user presses left and right, move the rectangle
-	group.setOnKeyPressed(event -> {
-		System.out.println(event);
-		if (event.getCode() == KeyCode.LEFT)  r.setX(r.getX() - 10.0);
-		if (event.getCode() == KeyCode.RIGHT) r.setX(r.getX() + 10.0);
-		// TODO bounds checking
-	    });
-
-        Scene scene = new Scene(group, 640, 480);
-        stage.setTitle("cs1302-arcade!");
-        stage.setScene(scene);
-	stage.sizeToScene();
-        stage.show();
-
-	// the group must request input focus to receive key events
-	// @see https://docs.oracle.com/javase/8/javafx/api/javafx/scene/Node.html#requestFocus--
-	group.requestFocus();
-
     } // start
 
     public static void main(String[] args) {
