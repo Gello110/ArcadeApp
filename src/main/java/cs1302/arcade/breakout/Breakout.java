6d4a88ac5cdfc7ab568a4b25be4771871350a82d
TIENZHENG ZHENG <tzheng@nike.cs.uga.edu>
2017-11-15 13:12:09 -0500

    Added skeleton for minesweeper and breakout

---
+++ src/main/java/cs1302/arcade/breakout/Breakout.java
@@ -0,0 +1,39 @@
+package cs1302.arcade.breakout;
+
+import javafx.scene.Scene;
+
+
+/**
+ *  Class representing a breakout game
+ */
+public class Breakout extends Game{
+    
+    
+
+    /**
+     *  Constructs a breakout game
+     */
+    public Breakout(){
+	super("Breakout");
+
+    }//Breakout
+
+    @Override
+    public void initScene(Scene scene){
+	
+
+    }//init Scene
+
+    @Override
+    public void updateScene(Scene scene){
+
+
+    }//updateScene
+
+    @Override
+    public boolean isOver(){
+
+
+    }//isOver
+
+}//Breakout
---
+++ src/main/java/cs1302/arcade/minesweeper/Minesweeper.java
@@ -0,0 +1,39 @@
+package cs1302.arcade.minesweeper;
+
+import javafx.scene.Scene;
+
+
+/**
+ *  Class representing a minesweeper game
+ */
+public class Minesweeper extends Game{
+    
+    
+
+    /**
+     *  Constructs a minesweeper game
+     */
+    public Minesweeper(){
+	super("Minesweeper");
+
+    }//Minesweeper
+
+    @Override
+    public void initScene(Scene scene){
+	
+
+    }//init Scene
+
+    @Override
+    public void updateScene(Scene scene){
+
+
+    }//updateScene
+
+    @Override
+    public boolean isOver(){
+
+
+    }//isOver
+
+}//Minsweeper
