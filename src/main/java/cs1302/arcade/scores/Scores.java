628250c6bea6272666553914b759b77d960b5d25
Angelo <angelozheng2000@gmail.com>
2017-12-10 16:21:32 -0500

    Added to high scores functionality

--- src/main/java/cs1302/arcade/scores/Scores.java
+++ src/main/java/cs1302/arcade/scores/Scores.java
@@ -1,10 +1,15 @@
 package cs1302.arcade.scores;
 
 
+import javafx.collections.FXCollections;
 import javafx.geometry.Pos;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -15,13 +20,15 @@ import java.util.ArrayList;
  * Class handling the high scores for minesweeper and breakout
  */
 public class Scores {
-    ArrayList<Entry> bScores;
-    ArrayList<Entry> mScores;
+    private ArrayList<Entry> bScores;
+    private ArrayList<Entry> mScores;
 
     /**
      * creates a score class
      */
     public Scores() {
+        bScores = new ArrayList<>();
+        mScores = new ArrayList<>();
         try {
             File b = new File("bScores.txt");
             File m = new File("mScores.txt");
@@ -61,14 +68,33 @@ public class Scores {
     /**
      * Makes an application modal window that displays the top high scores
      */
-    public void getScores(){
+    public void diplayScores(){
         Stage s = new Stage();
-        VBox box = new VBox(10);
-        box.setAlignment(Pos.CENTER);
+        VBox vbox = new VBox(10);
+        HBox hBox = new HBox(10);
+        vbox.setAlignment(Pos.CENTER);
 
-        TableView<String> table = new TableView<>();
-        TableColumn<String, String> name = new TableColumn<>("Name");
-        TableColumn<String, Integer> score = new TableColumn<>("Score");
+        //breakout scores
+        TableColumn<Entry, String> bname = new TableColumn<>("Name");
+        bname.setCellValueFactory(new PropertyValueFactory<Entry, String>("name"));
+        TableColumn<Entry, Integer> bscore = new TableColumn<>("Score");
+        bscore.setCellValueFactory(new PropertyValueFactory<Entry, Integer>("score"));
+
+
+        bname.setMinWidth(150);
+        bscore.setMinWidth(150);
+
+        TableView<Entry> btable = new TableView<>();//table of breakout scores
+        btable.getColumns().addAll(bname, bscore);
+        btable.setItems(FXCollections.observableArrayList(bScores));
+
+
+
+
+        Text title = new Text("High Scores");
+        title.setFont(new Font(30));
+
+        vbox.getChildren().addAll(title, btable);
 
 
     }
@@ -82,6 +108,27 @@ public class Scores {
      * @return
      */
     public boolean addScore(String game, String name, int score){
+        if(game.equals("minesweeper")){
+            //adds score to list of scores and sorts
+            mScores.add(new Entry(name,score));
+            mScores.sort((e1, e2) -> e2.compareTo(e1));
+
+            //truncates list of scores if list longer than 10
+            if(mScores.size() > 10){
+                mScores.subList(0,9);
+            }
+        }else if(game.equals("breakout")){
+            //adds score to list of scores and sorts
+            bScores.add(new Entry(name, score));
+            bScores.sort((e1, e2) -> e2.compareTo(e1));
+
+            //truncates list of scores if list longer than 10
+            if(bScores.size() > 10){
+                bScores.subList(0, 9);
+            }
+        }else{
+            return false;
+        }
         return true;
     }//addScore
 
